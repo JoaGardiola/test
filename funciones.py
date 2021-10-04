c157c7a5cd97c54833eb21787e10efbd6f621033
JoaGardiola <joa.gardiola@gmail.com>
2021-10-04 13:02:30 -0300

    llamada a funciones desde main

--- funciones.py
+++ funciones.py
@@ -6,4 +6,3 @@ def prod(a,b):
 
 def resta(a,b):
     print(a-b)
-    
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from funciones import *
+
 if __name__ == "__main__":
     print("Primer programa python")
+
+    suma(2,4)
+    prod(2,4)
